3502712605d2dff54b2d65d9933370250d6f77ca
rburkitt <raymondburkitt@hotmail.com>
2013-12-03 12:31:08 -0500

    Initial Commit

---
+++ ServiceNowRepository.cs
@@ -0,0 +1,404 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace LinqToServiceNow
+{
+    public class ServiceNowRepository<TServiceNow_cmdb_ci_, TGetRecords, TGetRecordsResponseGetRecordsResult>
+        where TServiceNow_cmdb_ci_ : new()
+        where TGetRecords : new()        
+    {
+        private TServiceNow_cmdb_ci_ proxyUser = new TServiceNow_cmdb_ci_();
+
+        private TGetRecords _filter = new TGetRecords();
+
+        private Func<TGetRecordsResponseGetRecordsResult, dynamic> _selectQuery;
+
+        private string _encodedQuery;
+
+        private void SetFilterProperty(string prop, string val)
+        {
+            Type t = _filter.GetType();
+            FieldInfo fInfo = t.GetField(prop);
+            if (fInfo != null)
+            {
+                fInfo.SetValue(_filter, val);
+            }
+            else
+            {
+                PropertyInfo pInfo = t.GetProperty(prop);
+                pInfo.SetValue(_filter, val, null);
+            }
+        }
+
+        private void CreateExpression(Utilities.ContinuationOperator continuation, Expression expr)
+        {
+            CreateExpression(continuation, expr, false);
+        }
+
+        private void CreateExpression(Utilities.ContinuationOperator continuation, Expression expr, bool neg)
+        {
+            ExpressionType[] operators = { ExpressionType.And, ExpressionType.Or };
+
+            if (expr.NodeType == ExpressionType.Call)
+            {
+                MethodCallExpression methodCall = (expr as MethodCallExpression);
+
+                if (methodCall.Method.Name == "Contains" && methodCall.Arguments[0].NodeType == ExpressionType.NewArrayInit)
+                    CreateContainsExpression(continuation, expr as MethodCallExpression, neg);
+                else
+                    CreateSimpleMethodCall(continuation, methodCall, neg);
+            }
+            else if (expr.NodeType == ExpressionType.Not)
+            {
+                UnaryExpression unaryExpr = expr as UnaryExpression;
+                CreateExpression(continuation, unaryExpr.Operand, true);
+            }
+            else
+                CreateSimpleExpression(continuation, expr as BinaryExpression, neg);
+        }
+
+        private void CreateSimpleExpression(Utilities.ContinuationOperator continuation, BinaryExpression binExpr, bool neg)
+        {
+            string fieldName = "";
+            string myValue = "";
+
+            ExpressionType oper = binExpr.NodeType;
+
+            Action FlipOperator = () => 
+            {
+                if( oper == ExpressionType.GreaterThan)
+                    oper = ExpressionType.LessThan;  
+
+                if(oper == ExpressionType.GreaterThanOrEqual)
+                    oper = ExpressionType.LessThanOrEqual;
+
+                if(oper == ExpressionType.LessThan)
+                    oper = ExpressionType.GreaterThan;
+ 
+                if (oper == ExpressionType.LessThanOrEqual)
+                    oper = ExpressionType.GreaterThanOrEqual;
+            };
+
+            ExpressionType[] binOperators = { ExpressionType.And, ExpressionType.Or, ExpressionType.AndAlso, ExpressionType.OrElse };
+
+            if (binOperators.Contains(binExpr.NodeType))
+            {
+                CreateExpression(continuation, binExpr.Left, neg);
+                CreateExpression((Utilities.ContinuationOperator)binExpr.NodeType, binExpr.Right, neg);
+            }
+            else
+            {
+                if (binExpr.Left.NodeType == ExpressionType.MemberAccess) // flip expression
+                {
+                    fieldName = GetFieldName(binExpr.Left);
+                    myValue = GetFieldValue(binExpr.Right);
+                }
+                else if( binExpr.Left.NodeType == ExpressionType.Constant) // flip expression
+                 {
+                    fieldName = GetFieldName(binExpr.Right);
+                    myValue = GetFieldValue(binExpr.Left);
+                    FlipOperator();
+                 }
+                 else if( binExpr.Left.NodeType == ExpressionType.Call)
+                 {
+                    MethodCallExpression methodCall = binExpr.Left as MethodCallExpression;
+                    if( methodCall.Method.Name == "CompareString")
+                    {
+                        if (methodCall.Arguments[0].NodeType == ExpressionType.MemberAccess)
+                        {
+                            fieldName = GetFieldName(methodCall.Arguments[1]);
+                            myValue = GetFieldValue(methodCall.Arguments[0]);
+                        }
+                        else
+                        {
+                            fieldName = GetFieldName(methodCall.Arguments[0]);
+                            myValue = GetFieldValue(methodCall.Arguments[1]);
+                        }
+                    }
+                    else if( methodCall.Arguments[0].NodeType == ExpressionType.Constant) //flip expression
+                    {
+                        fieldName = GetFieldName(binExpr.Right);
+                        myValue = GetFieldValue(binExpr.Left);
+                        FlipOperator();
+                    }
+                    else
+                    {
+                        fieldName = GetFieldName(binExpr.Left);
+                        myValue = GetFieldValue(binExpr.Right);
+                    }
+                 }
+            }
+
+            EncodeQuery(continuation, fieldName, (Utilities.RepoExpressionType)oper, myValue, neg);
+        }
+
+        private void CreateContainsExpression(Utilities.ContinuationOperator continuation, MethodCallExpression methodCall, bool neg)
+        {
+            string fieldName = "";
+            string myValue = "";
+
+            fieldName = GetFieldName(methodCall.Arguments[1]);
+            myValue = GetFieldValue(methodCall.Arguments[0]);
+
+            if (string.IsNullOrEmpty(fieldName) | string.IsNullOrEmpty(myValue))
+            {
+                CreateExpression(continuation, methodCall, neg);
+            }
+
+            EncodeQuery(continuation, fieldName, Utilities.RepoExpressionType.IN, myValue, neg);
+        }
+
+        private void CreateSimpleMethodCall(Utilities.ContinuationOperator continuation, MethodCallExpression methodCall, bool neg)
+        {
+            string fieldName = "";
+            string myValue = "";
+
+            fieldName = GetFieldName(methodCall.Object);
+            myValue = GetFieldValue(methodCall.Arguments[0]);
+
+            if(string.IsNullOrEmpty(fieldName) || string.IsNullOrEmpty(myValue))
+                CreateExpression(continuation, methodCall, neg);
+
+            EncodeQuery(continuation, fieldName, Utilities.GetRepoExpressionType(methodCall.Method.Name), myValue, neg);
+        }
+
+        private string GetFieldName(Expression expr)
+        {
+            string fieldname = "";
+
+            if (expr.NodeType == ExpressionType.MemberAccess)
+                fieldname = GetPropertyName(expr);
+
+            if (expr.NodeType == ExpressionType.Call && (expr as MethodCallExpression).Method.Name == "Parse")
+                fieldname = GetPropertyName((expr as MethodCallExpression).Arguments[0]);
+
+            return fieldname;
+        }
+
+        private string GetFieldValue(Expression expr)
+        {
+            string fieldvalue = "";
+
+            if (expr.NodeType == ExpressionType.Constant)
+                fieldvalue = expr.ToString().Replace("\"", "");
+
+            if (expr.NodeType == ExpressionType.MemberAccess)
+                fieldvalue = GetPropertyName(expr);
+
+            if (expr.NodeType == ExpressionType.Call && (expr as MethodCallExpression).Method.Name == "Parse")
+                fieldvalue = (expr as MethodCallExpression).Arguments[0].ToString().Replace("\"", "");
+
+            if (expr.NodeType == ExpressionType.Convert)
+                fieldvalue = GetFieldValue((expr as UnaryExpression).Operand);
+
+            if (expr.NodeType == ExpressionType.NewArrayInit)
+                fieldvalue = string.Join(",", (expr as NewArrayExpression).Expressions.Select(o => o.ToString().Replace("\"", "")).ToArray());
+
+            return fieldvalue;
+        }
+
+        private string GetPropertyName(Expression propertyRefExpr)
+        {
+            if (propertyRefExpr == null)
+            {
+                throw new ArgumentNullException("propertyRefExpr", "propertyRefExpr is null.");
+            }
+
+            if (propertyRefExpr.NodeType == ExpressionType.Constant)
+            {
+                return propertyRefExpr.ToString();
+            }
+
+            MemberExpression memberExpr = propertyRefExpr as MemberExpression;
+            if (memberExpr == null)
+            {
+                UnaryExpression unaryExpr = propertyRefExpr as UnaryExpression;
+                if (unaryExpr != null && unaryExpr.NodeType == ExpressionType.Convert)
+                {
+                    memberExpr = unaryExpr.Operand as MemberExpression;
+                }
+            }
+
+            if (memberExpr != null && memberExpr.Member.MemberType == System.Reflection.MemberTypes.Property)
+            {
+                return memberExpr.Member.Name;
+            }
+
+            if (memberExpr != null && memberExpr.Member.MemberType == System.Reflection.MemberTypes.Field)
+            {
+                return memberExpr.Member.Name;
+            }
+
+            throw new ArgumentException("No property reference expression was found.", "propertyRefExpr");
+        }
+
+        private void EncodeQuery(Utilities.ContinuationOperator continuationOperator, string fieldName, Utilities.RepoExpressionType myOperator, string myValue, bool neg)
+        {
+            if (string.IsNullOrEmpty(fieldName) | string.IsNullOrEmpty(myValue))
+            {
+                return;
+            }
+
+            string oper = Utilities.GetRepoExpressionType(myOperator);
+
+            if (neg)
+                oper = Utilities.NegateRepoExpressionType(myOperator);
+
+            string query = string.Format("{0}{1}{2}", fieldName, oper, myValue);
+
+            if (!string.IsNullOrEmpty(_encodedQuery))
+            {
+                query = Utilities.GetContinuationOperator(continuationOperator) + query;
+            }
+
+            _encodedQuery += query;
+
+            SetFilterProperty("__encoded_query", _encodedQuery);
+        }
+
+        private void SetOrdering(string order, Expression<Func<TGetRecordsResponseGetRecordsResult, dynamic>> field)
+        {
+            if (field.Body.NodeType == ExpressionType.New)
+            {
+                SetFilterProperty(order, string.Join(",", (field.Body as NewExpression).Arguments.Select(o => GetPropertyName(o)).ToArray()));
+            }
+            else
+            {
+                SetFilterProperty(order, GetPropertyName(field.Body));
+            }
+        }
+
+        public dynamic First(Expression<Func<TGetRecordsResponseGetRecordsResult, bool>> stmt)
+        {
+            SetFilterProperty("__limit", "1");
+            Where(stmt);
+            return ToArray().First();
+        }
+
+        public ServiceNowRepository<TServiceNow_cmdb_ci_, TGetRecords, TGetRecordsResponseGetRecordsResult> Take(int count)
+        {
+            SetFilterProperty("__last_row", count.ToString());
+            return this;
+        }
+
+        public ServiceNowRepository<TServiceNow_cmdb_ci_, TGetRecords, TGetRecordsResponseGetRecordsResult> Skip(int count)
+        {
+            SetFilterProperty("__first_row", count.ToString());
+            return this;
+        }
+
+        public ServiceNowRepository<TServiceNow_cmdb_ci_, TGetRecords, TGetRecordsResponseGetRecordsResult> Range(int start, int last)
+        {
+            SetFilterProperty("__first_row", (start - 1).ToString());
+            SetFilterProperty("__last_row", ((start - 1) + last).ToString());
+            return this;
+        }
+
+        public dynamic[] ToArray()
+        {
+            Type t = proxyUser.GetType();
+            MethodInfo methodInfo = t.GetMethod("getRecords");
+
+            TGetRecordsResponseGetRecordsResult[] ret = (TGetRecordsResponseGetRecordsResult[])methodInfo.Invoke(proxyUser, new object[] { _filter });
+
+            _encodedQuery = string.Empty;
+            _filter = new TGetRecords();
+
+            if(_selectQuery != null)
+                return ret.Select(o => _selectQuery(o)).ToArray();
+            else
+                return ret.Cast<dynamic>().ToArray();
+        }
+
+        public List<dynamic> ToList()
+        {
+            return ToArray().ToList();
+        }
+
+        public ServiceNowRepository<TServiceNow_cmdb_ci_, TGetRecords, TGetRecordsResponseGetRecordsResult> @Select<U>(Func<TGetRecordsResponseGetRecordsResult, U> selector)
+        {
+            _selectQuery = (s) => selector(s);
+            return this;
+        }
+
+        public ServiceNowRepository<TServiceNow_cmdb_ci_, TGetRecords, TGetRecordsResponseGetRecordsResult> Where(Expression<Func<TGetRecordsResponseGetRecordsResult, bool>> stmt)
+        {
+            CreateExpression(Utilities.ContinuationOperator.And, stmt.Body);
+            return this;
+        }
+
+        public TGetRecordsResponseGetRecordsResult First()
+        {
+            return First((o) => true);
+        }
+
+        public TGetRecordsResponseGetRecordsResult Single()
+        {
+            return First();
+        }
+
+        public TGetRecordsResponseGetRecordsResult Single(Expression<Func<TGetRecordsResponseGetRecordsResult, bool>> stmt)
+        {
+            return First(stmt);
+        }
+
+        public bool Any()
+        {
+            return First() != null;
+        }
+
+        public bool Any(Expression<Func<TGetRecordsResponseGetRecordsResult, bool>> stmt)
+        {
+            return First(stmt) != null;
+        }
+
+        public ServiceNowRepository<TServiceNow_cmdb_ci_, TGetRecords, TGetRecordsResponseGetRecordsResult> OrderBy(Expression<Func<TGetRecordsResponseGetRecordsResult, dynamic>> field)
+        {
+            SetOrdering("__order_by", field);
+
+            return this;
+        }
+
+        public ServiceNowRepository<TServiceNow_cmdb_ci_, TGetRecords, TGetRecordsResponseGetRecordsResult> OrderByDescending(Expression<Func<TGetRecordsResponseGetRecordsResult, dynamic>> field)
+        {
+            SetOrdering("__order_by_desc", field);
+
+            return this;
+        }
+
+        public dynamic Insert<TInsert>(TInsert _insert)
+        {
+            Type t = proxyUser.GetType();
+            MethodInfo methodInfo = t.GetMethod("insert");
+            return methodInfo.Invoke(proxyUser, new object[]{_insert});
+        }
+
+        public dynamic Update<TUpdate>(TUpdate _update) 
+        {
+            Type t  = proxyUser.GetType();
+            MethodInfo methodInfo = t.GetMethod("update");
+            return methodInfo.Invoke(proxyUser, new object[]{_update});
+        }
+
+        public dynamic Delete<TDelete>(TDelete _delete) where TDelete : new()
+        {
+            Type t  = proxyUser.GetType();
+            MethodInfo methodInfo = t.GetMethod("deleteRecord");
+            return methodInfo.Invoke(proxyUser, new object[] { _delete });
+        }
+
+        public IEnumerator<ServiceNowRepository<TServiceNow_cmdb_ci_, TGetRecords, TGetRecordsResponseGetRecordsResult>> GetEnumerator()
+        {
+            return ToList().Cast<ServiceNowRepository<TServiceNow_cmdb_ci_, TGetRecords, TGetRecordsResponseGetRecordsResult>>().GetEnumerator();
+        }
+
+        public IEnumerator GetEnumerator1()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ Utilities.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace LinqToServiceNow
+{
+    class Utilities
+    {
+        public enum RepoExpressionType
+        {
+            [Description("=")]
+            Equal = ExpressionType.Equal,
+            [Description("!=")]
+            NotEqual = ExpressionType.NotEqual,
+            [Description(">")]
+            GreaterThan = ExpressionType.GreaterThan,
+            [Description("<")]
+            LessThan = ExpressionType.LessThan,
+            [Description(">=")]
+            GreaterThanOrEqual = ExpressionType.GreaterThanOrEqual,
+            [Description("<=")]
+            LessThanOrEqual = ExpressionType.LessThanOrEqual,
+            @IN = 100,
+            @STARTSWITH = 200,
+            @CONTAINS = 300,
+            @ENDSWITH = 400
+        }
+
+        public enum ContinuationOperator
+        {
+            [Description("^")]
+            @And = ExpressionType.And,
+            [Description("^")]
+            @AndAlso = ExpressionType.AndAlso,
+            [Description("^OR")]
+            @Or = ExpressionType.Or,
+            [Description("^OR")]
+            @OrElse = ExpressionType.OrElse
+        }
+
+        private static string GetOperator<T>(T en)
+        {
+	        Type type = en.GetType();
+
+	        MemberInfo[] memInfo = type.GetMember(en.ToString());
+
+	        if (memInfo != null & memInfo.Length > 0) {
+		        object[] attrs = memInfo[0].GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+		        if (attrs != null & attrs.Length > 0) {
+			        return ((DescriptionAttribute)attrs[0]).Description;
+		        }
+	        }
+
+	        return en.ToString();
+        }
+
+        public static string GetContinuationOperator(ContinuationOperator en)
+        {
+	        return GetOperator<ContinuationOperator>(en);
+        }
+
+        public static string GetRepoExpressionType(RepoExpressionType en)
+        {
+	        return GetOperator<RepoExpressionType>(en);
+        }
+
+        public static RepoExpressionType GetRepoExpressionType(string en)
+        {
+            RepoExpressionType val = (RepoExpressionType)Enum.Parse(typeof(RepoExpressionType), en.ToUpper());
+            return val;
+        }
+
+        public static string NegateRepoExpressionType(RepoExpressionType en)
+        {
+            string oper = GetRepoExpressionType(en);
+
+            switch(en)
+            {
+                case RepoExpressionType.IN:
+                    oper = "NOT IN";
+                    break;
+                case RepoExpressionType.CONTAINS:
+                    oper = "DOES NOT CONTAIN";
+                    break;
+                case RepoExpressionType.STARTSWITH:
+                    oper = "NOT STARTSWITH";
+                    break;
+                case RepoExpressionType.ENDSWITH:
+                    oper = "NOT ENDSWITH";
+                    break;
+                default:
+                    oper = GetRepoExpressionType(FlipRepoExpressionType(en));
+                    break;
+            }
+
+            return oper;
+
+        }
+
+        public static RepoExpressionType FlipRepoExpressionType(RepoExpressionType en)
+        {
+
+            RepoExpressionType oper = default(RepoExpressionType);
+
+            switch (en)
+            {
+                case RepoExpressionType.GreaterThan:
+                    oper = RepoExpressionType.LessThan;
+                    break;
+                case RepoExpressionType.GreaterThanOrEqual:
+                    oper = RepoExpressionType.LessThanOrEqual;
+                    break;
+                case RepoExpressionType.LessThan:
+                    oper = RepoExpressionType.GreaterThan;
+                    break;
+                case RepoExpressionType.LessThanOrEqual:
+                    oper = RepoExpressionType.GreaterThanOrEqual;
+                    break;
+            }
+
+            return oper;
+
+        }
+    }
+}
